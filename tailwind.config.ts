37d8ee1a1054f450bc0b9d287c786a97a46b5a18
chevybowtie <paul.sturm@cotton-software.com>
2026-02-08 15:21:39 -0600

    Remove darkMode configuration from Tailwind CSS setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./src/renderer/index.html", "./src/renderer/**/*.{vue,js,ts,jsx,tsx}"],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
     },
